6e40696c9bf0544ac78318b6120984562b93a694
Chris Von Bargen <christopher.vonbargen@schrodinger.com>
2025-05-23 14:45:53 -0400

    rsync_rdkit_extensions

---
+++ src/schrodinger/rdkit_extensions/cg_monomer_database.cpp
@@ -0,0 +1,199 @@
+#include "schrodinger/rdkit_extensions/cg_monomer_database.h"
+
+#include <array>
+#include <memory>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/noncopyable.hpp>
+#include <boost/filesystem.hpp>
+#include <fmt/format.h>
+
+#include "schrodinger/rdkit_extensions/coarse_grain.h" // ChainType
+
+#include "sqlite3.h"
+
+using namespace std::string_view_literals;
+
+namespace schrodinger
+{
+namespace rdkit_extensions
+{
+
+std::optional<std::string> get_custom_monomer_db_path()
+{
+#ifdef __EMSCRIPTEN__
+    throw std::logic_error(
+        "CG Monomers are not yet supported in WASM Sketcher");
+#else
+    auto custom_db_path = getenv(CUSTOM_MONOMER_DB_PATH_ENV_VAR.c_str());
+    if (custom_db_path) {
+        return std::make_optional<std::string>(custom_db_path);
+    }
+    return std::nullopt;
+#endif
+}
+
+std::optional<std::string> get_cg_monomer_db_path()
+{
+#ifdef __EMSCRIPTEN__
+    throw std::logic_error(
+        "CG Monomers are not yet supported in WASM Sketcher");
+#else
+    auto custom_db_path = get_custom_monomer_db_path();
+    if (custom_db_path.has_value() &&
+        boost::filesystem::exists(*custom_db_path)) {
+        return custom_db_path;
+    }
+    auto db_path = getenv(DEFAULT_MONOMER_DB_PATH_ENV_VAR.c_str());
+    if (db_path) {
+        return std::make_optional<std::string>(db_path);
+    }
+    return std::nullopt;
+#endif
+}
+
+cg_monomer_database::cg_monomer_database(std::string_view database_path)
+{
+    static constexpr std::string_view err_msg_template{
+        "Problem opening database: '{}'"};
+
+// For now, we only support non-emscripten builds.
+#ifdef __EMSCRIPTEN__
+    database_path = ":memory:";
+#endif
+    if (auto rc = sqlite3_open_v2(database_path.data(), &m_db,
+                                  SQLITE_OPEN_READONLY, nullptr);
+        rc != SQLITE_OK) {
+        throw std::runtime_error(fmt::format(err_msg_template, database_path));
+    }
+}
+
+namespace
+{
+template <class value_getter_t> [[nodiscard]] auto
+get_info_from_database(sqlite3* db, const std::string& sql_command_template,
+                       value_getter_t return_val_converter)
+{
+    auto sqlite3_stmt_deleter = [](auto* stmt) { sqlite3_finalize(stmt); };
+    std::unique_ptr<sqlite3_stmt, decltype(sqlite3_stmt_deleter)> statement{
+        nullptr, sqlite3_stmt_deleter};
+
+    // storing this variable here instead of on the db class to improve
+    // readability.
+    //
+    // NOTE: THE ORDER OF THE TABLE NAMES MATTERS. WE WANT TO PRIORITIZE ENTRIES
+    // IN THE USER_MONOMERS TABLE OVER ENTRIES IN THE CORE_MONOMERS TABLE.
+    constexpr std::array<std::string_view, 2> table_names{"user_monomers",
+                                                          "core_monomers"};
+
+    // NOTE: Return the first result you see
+    for (const auto& table : table_names) {
+        auto sql_command =
+            fmt::format(fmt::runtime(sql_command_template), table);
+        sqlite3_stmt* stmt = nullptr;
+        if (auto rc =
+                sqlite3_prepare_v2(db, sql_command.c_str(), -1, &stmt, NULL);
+            rc != SQLITE_OK) {
+            continue;
+        }
+
+        statement.reset(stmt);
+        if (auto rc = sqlite3_step(stmt);
+            rc != SQLITE_DONE && rc != SQLITE_OK) {
+            return return_val_converter(stmt);
+        };
+    }
+
+    // this is an easier way to return nullopt from this templated function
+    return std::invoke_result_t<value_getter_t, sqlite3_stmt*>{};
+}
+} // namespace
+
+cg_monomer_database::~cg_monomer_database()
+{
+    sqlite3_close_v2(m_db);
+}
+
+cg_monomer_database::string_t
+cg_monomer_database::get_monomer_smiles(std::string monomer_id,
+                                        ChainType monomer_type)
+{
+    auto get_sql_command = [&]() -> std::string {
+        static constexpr std::string_view template_{
+            "SELECT {0} FROM {{}} WHERE {1}='{2}' AND {3}='{4}';"};
+        static constexpr std::string_view smiles_column{"SMILES"};
+        static constexpr std::string_view symbol_column{"SYMBOL"};
+        static constexpr std::string_view type_column{"POLYMERTYPE"};
+
+        auto type_value = to_string(monomer_type);
+        boost::algorithm::trim(monomer_id);
+
+        return fmt::format(template_, smiles_column, type_column, type_value,
+                           symbol_column, monomer_id);
+    };
+
+    auto smiles_getter = [](sqlite3_stmt* stmt) -> string_t {
+        auto result = sqlite3_column_text(stmt, 0);
+        return std::make_optional(reinterpret_cast<const char*>(result));
+    };
+
+    return get_info_from_database(m_db, get_sql_command(), smiles_getter);
+}
+
+[[nodiscard]] cg_monomer_database::helm_info_t
+cg_monomer_database::get_helm_info(const std::string& pdb_code)
+{
+    auto get_sql_command = [&]() -> std::string {
+        static constexpr std::string_view template_{
+            "SELECT {0}, {1}, {2} FROM {{}} WHERE {3}='{4}';"};
+        static constexpr std::string_view symbol_column{"SYMBOL"};
+        static constexpr std::string_view type_column{"POLYMERTYPE"};
+        static constexpr std::string_view smiles_column{"SMILES"};
+        static constexpr std::string_view pdb_code_column{"PDBCODE"};
+
+        return fmt::format(template_, symbol_column, type_column, smiles_column,
+                           pdb_code_column, pdb_code);
+    };
+
+    auto helm_info_getter = [](sqlite3_stmt* stmt) -> helm_info_t {
+        // clang-format off
+        auto symbol = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
+        auto polymer_type = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
+        auto smiles = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2));
+        helm_info_t::value_type helm_info{symbol, smiles, to_chain_type(polymer_type)};
+        return std::make_optional(helm_info);
+        // clang-format on
+    };
+
+    return get_info_from_database(m_db, get_sql_command(), helm_info_getter);
+}
+
+[[nodiscard]] cg_monomer_database::string_t
+cg_monomer_database::get_pdb_code(const std::string& helm_symbol,
+                                  ChainType monomer_type)
+{
+    auto get_sql_command = [&]() -> std::string {
+        static constexpr std::string_view template_{
+            "SELECT {0} FROM {{}} WHERE {1}='{2}' AND {3}='{4}';"};
+        static constexpr std::string_view pdb_code_column{"PDBCODE"};
+        static constexpr std::string_view symbol_column{"SYMBOL"};
+        static constexpr std::string_view type_column{"POLYMERTYPE"};
+
+        return fmt::format(template_, pdb_code_column, symbol_column,
+                           helm_symbol, type_column, to_string(monomer_type));
+    };
+
+    auto pdb_code_getter = [](sqlite3_stmt* stmt) -> string_t {
+        auto result = sqlite3_column_text(stmt, 0);
+        return std::make_optional(reinterpret_cast<const char*>(result));
+    };
+
+    return get_info_from_database(m_db, get_sql_command(), pdb_code_getter);
+}
+
+} // namespace rdkit_extensions
+} // namespace schrodinger
---
+++ test/schrodinger/rdkit_extensions/test_rdkit_to_fasta.cpp
@@ -0,0 +1,149 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE test_rdkit_to_fasta
+
+#include <boost/test/data/test_case.hpp>
+#include <rdkit/GraphMol/FileParsers/SequenceParsers.h>
+#include <rdkit/GraphMol/ROMol.h>
+#include <rdkit/GraphMol/RWMol.h>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "schrodinger/rdkit_extensions/fasta/to_rdkit.h"
+#include "schrodinger/rdkit_extensions/fasta/to_string.h"
+#include "schrodinger/rdkit_extensions/helm/to_rdkit.h"
+#include "schrodinger/rdkit_extensions/helm/to_string.h"
+
+#include "schrodinger/test/checkexceptionmsg.h" // TEST_CHECK_EXCEPTION_MSG_SUBSTR
+
+namespace bdata = boost::unit_test::data;
+
+BOOST_AUTO_TEST_CASE(TestAtomisticConversion)
+{
+    auto mol = std::unique_ptr<::RDKit::RWMol>(::RDKit::FASTAToMol("AAPL"));
+    TEST_CHECK_EXCEPTION_MSG_SUBSTR(
+        fasta::rdkit_to_fasta(*mol), std::invalid_argument,
+        "FASTA conversions with atomistic mols are currently unsupported");
+}
+
+BOOST_DATA_TEST_CASE(
+    TestUnsupportedFeatures,
+    bdata::make(std::vector<std::string>{
+        R"(PEPTIDE1{A.G.J.K.L}$$$"something"$V2.0)", // extended
+                                                     // annotations
+        "PEPTIDE1{A.G.C.K.L}|PEPTIDE2{A.G.C.K.L}$$G1(PEPTIDE1+"
+        "PEPTIDE2)$$V2.0",                         // polymer groups
+        "PEPTIDE1{A.G'3'}$$$$V2.0",                // monomer repeats
+        "PEPTIDE1{A.G.C.K.L}|BLOB1{Bead}$$$$V2.0", // blob
+                                                   // polymers
+        "PEPTIDE1{A.G.C.K.L}|CHEM1{[dR]}$$$$V2.0", // unknown
+                                                   // polymers
+    }),
+    input_helm)
+{
+    auto mol = helm::helm_to_rdkit(input_helm);
+    TEST_CHECK_EXCEPTION_MSG_SUBSTR(fasta::rdkit_to_fasta(*mol),
+                                    std::invalid_argument,
+                                    "currently unsupported");
+}
+
+BOOST_DATA_TEST_CASE(TestUnsupportedNucleotides,
+                     bdata::make(std::vector<std::string>{
+                         "RNA1{R}$$$$V2.0",     // missing subunit components
+                         "RNA1{X(A)P}$$$$V2.0", // unsupported sugar
+                         "RNA1{R(A)P.[dR](A)P}$$$$V2.0", // non-uniform sugars
+                         "RNA1{R(A)R}$$$$V2.0",          // missing phosphate
+                         "RNA1{R.R}$$$$V2.0",            // missing base
+
+                     }),
+                     input_helm)
+{
+    auto mol = helm::helm_to_rdkit(input_helm);
+    BOOST_CHECK_THROW(fasta::rdkit_to_fasta(*mol), std::invalid_argument);
+}
+
+BOOST_DATA_TEST_CASE(TestUnsupportedMonomers,
+                     bdata::make(std::vector<std::string>{
+                         "PEPTIDE1{A.A.(E+Q).L}$$$$V2.0",
+                         "RNA1{R(A)P.R((C+G+T+U))P}$$$$V2.0",
+                         "RNA1{R(Z)P}$$$$V2.0",
+                     }),
+                     input_helm)
+{
+    auto mol = helm::helm_to_rdkit(input_helm);
+    TEST_CHECK_EXCEPTION_MSG_SUBSTR(fasta::rdkit_to_fasta(*mol),
+                                    std::invalid_argument,
+                                    "Unsupported monomer");
+}
+
+BOOST_DATA_TEST_CASE(
+    TestValidPeptides,
+    bdata::make(std::vector<std::string>{
+        ">\nAAOL\n",
+        ">something\nARGXCKXEDA\n",
+        ">some description\nAAA\n",
+        ">some description\nAAA\n>something\nDDD\n",
+    }) ^
+        bdata::make(std::vector<std::string>{
+            "PEPTIDE1{A.A.O.L}$$$$V2.0",
+            R"(PEPTIDE1{A.R.G.X.C.K.X.E.D.A}"something"$$$$V2.0)",
+            R"(PEPTIDE1{A.A.A}"some description"$$$$V2.0)",
+            R"(PEPTIDE1{A.A.A}"some description"|PEPTIDE2{D.D.D}"something"$$$$V2.0)",
+        }),
+    input_fasta, input_helm)
+{
+    auto mol = fasta::peptide_fasta_to_rdkit(input_fasta);
+    BOOST_TEST(fasta::rdkit_to_fasta(*mol) == input_fasta);
+    BOOST_TEST(helm::rdkit_to_helm(*mol) == input_helm);
+}
+
+BOOST_DATA_TEST_CASE(
+    TestValidNucleotides,
+    (bdata::make(std::vector<std::string>{
+         ">\nAAG\n",
+         ">something\nAAU\n",
+         ">some description\nAAA\n",
+         ">some description\nAAA\n>something\nTTT\n",
+     }) ^
+     bdata::make(std::vector<std::string>{
+         "RNA1{R(A)P.R(A)P.R(G)P}$$$$V2.0",
+         R"(RNA1{R(A)P.R(A)P.R(U)P}"something"$$$$V2.0)",
+         R"(RNA1{R(A)P.R(A)P.R(A)P}"some description"$$$$V2.0)",
+         R"(RNA1{R(A)P.R(A)P.R(A)P}"some description"|RNA2{R(T)P.R(T)P.R(T)P}"something"$$$$V2.0)",
+     })) ^
+        bdata::make(std::vector<std::string>{
+            "RNA1{[dR](A)P.[dR](A)P.[dR](G)P}$$$$V2.0",
+            R"(RNA1{[dR](A)P.[dR](A)P.[dR](U)P}"something"$$$$V2.0)",
+            R"(RNA1{[dR](A)P.[dR](A)P.[dR](A)P}"some description"$$$$V2.0)",
+            R"(RNA1{[dR](A)P.[dR](A)P.[dR](A)P}"some description"|RNA2{[dR](T)P.[dR](T)P.[dR](T)P}"something"$$$$V2.0)",
+        }),
+    input_fasta, rna_helm, dna_helm)
+{
+    auto mol = fasta::rna_fasta_to_rdkit(input_fasta);
+    BOOST_TEST(fasta::rdkit_to_fasta(*mol) == input_fasta);
+    BOOST_TEST(helm::rdkit_to_helm(*mol) == rna_helm);
+
+    mol = fasta::dna_fasta_to_rdkit(input_fasta);
+    BOOST_TEST(fasta::rdkit_to_fasta(*mol) == input_fasta);
+    BOOST_TEST(helm::rdkit_to_helm(*mol) == dna_helm);
+}
+
+BOOST_DATA_TEST_CASE(
+    TestWritingStructuresWithInterPolymerAndNonlinearConnections,
+    bdata::make(std::vector<std::string>{
+        "PEPTIDE1{A.D(C)P.G}$$$$V2.0",
+        "PEPTIDE1{A.G.C.K.L}$PEPTIDE1,PEPTIDE1,5:R2-1:R1$$$V2."
+        "0",
+        R"(PEPTIDE1{A.A.A.C.C}|PEPTIDE2{D.L.L.L.V.V.V}|PEPTIDE3{C.D.D}"Test annotation"$PEPTIDE1,PEPTIDE3,5:R2-1:R1|PEPTIDE3,PEPTIDE2,3:R2-1:R1$$$V2.0)",
+    }) ^ bdata::make(std::vector<std::string>{
+             ">\nADCPG\n", // branch monomers stripped
+             ">\nAGCKL\n", // polymer cycle removed
+             ">\nAAACC\n>\nDLLLVVV\n>Test annotation\nCDD\n", // multiple
+                                                              // connections
+         }),
+    input_helm, expected_fasta)
+{
+    auto mol = helm::helm_to_rdkit(input_helm);
+    BOOST_TEST(fasta::rdkit_to_fasta(*mol) == expected_fasta);
+}
