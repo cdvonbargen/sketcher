a686ac82a5c96820df242fa5c29432040878c03b
Chris Von Bargen <christopher.vonbargen@schrodinger.com>
2025-06-01 21:01:07 -0700

    more

--- src/schrodinger/sketcher/example_widget.cpp
+++ src/schrodinger/sketcher/example_widget.cpp
@@ -16,10 +16,7 @@ ExampleWidget::ExampleWidget(QWidget* parent) : QWidget(parent)
 {
     m_ui.reset(new Ui::ExampleWidgetForm());
     m_ui->setupUi(this);
-    auto mol = rdkit_extensions::to_rdkit("C1=CC=CC=C1");
-    auto smiles =
-        rdkit_extensions::to_string(*mol, rdkit_extensions::Format::SMILES);
-    m_ui->label->setText(QString::fromStdString(smiles));
+    m_ui->label->setText("Hello World!");
 }
 
 ExampleWidget::~ExampleWidget() = default;
