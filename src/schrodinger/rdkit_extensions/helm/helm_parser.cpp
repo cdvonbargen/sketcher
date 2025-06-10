9391a321837cc88d544007e3f7e16573bf0a0840
Chris Von Bargen <christopher.vonbargen@schrodinger.com>
2025-06-09 21:26:14 -0400

    cp rdkit_extensions

---
+++ src/schrodinger/rdkit_extensions/helm/helm_parser.cpp
@@ -0,0 +1,236 @@
+#include "schrodinger/rdkit_extensions/helm/helm_parser.h"
+
+#include <algorithm>
+#include <array>
+#include <fmt/format.h>
+#include <iterator>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "schrodinger/rdkit_extensions/helm/generated/helm_parser.tab.hh"
+#include "schrodinger/rdkit_extensions/helm/validation.h"
+
+static void trim_string_view(std::string_view& value)
+{
+    while (!value.empty()) {
+        if (std::isspace(value.front())) {
+            value.remove_prefix(1);
+        } else if (std::isspace(value.back())) {
+            value.remove_suffix(1);
+        } else {
+            break;
+        }
+    }
+};
+
+[[nodiscard]] static std::array<std::string_view, 3>
+parse_extended_annotations_and_version(const std::string_view input_helm)
+{
+    if (std::ranges::count(input_helm, '$') < 2) {
+        return {input_helm, "", ""};
+    }
+
+    auto ext_annotations_end = input_helm.rfind('$');
+    auto ext_annotations_start = input_helm.rfind('$', ext_annotations_end - 1);
+
+    auto helm_prefix = input_helm.substr(0, ext_annotations_start + 1);
+    auto extended_annotations =
+        input_helm.substr(ext_annotations_start + 1,
+                          ext_annotations_end - ext_annotations_start - 1);
+    auto helm_version = ext_annotations_end == input_helm.size() - 1
+                            ? ""
+                            : input_helm.substr(ext_annotations_end + 1);
+
+    return {helm_prefix, extended_annotations, helm_version};
+}
+
+namespace helm
+{
+
+HelmParser::HelmParser(const std::string_view input_helm) : m_input(input_helm)
+{
+    // strip surrounding whitespace
+    trim_string_view(m_input);
+}
+
+helm_info HelmParser::parse()
+{
+    auto [input_helm_prefix, extended_annotations, helm_version] =
+        parse_extended_annotations_and_version(m_input);
+
+    std::istringstream stream(
+        std::string{input_helm_prefix.data(), input_helm_prefix.size()});
+    helm::TokenScanner scanner(&stream, m_input);
+    helm::TokenParser parser(scanner, *this);
+
+    // success return 0
+    if (parser.parse() != 0) {
+        std::stringstream ss;
+        std::copy(m_errors.begin(), m_errors.end(),
+                  std::ostream_iterator<std::string>(ss, "\n"));
+        throw std::invalid_argument(ss.str());
+    }
+
+    add_extended_annotations(extended_annotations);
+    add_helm_version(helm_version);
+
+    validate_parsed_info(m_parsed_info, *this);
+    if (hasErrors()) {
+        std::stringstream ss;
+        ss << "Parsing failed because of the following: \n";
+        std::copy(m_errors.begin(), m_errors.end(),
+                  std::ostream_iterator<std::string>(ss, "\n"));
+        throw std::invalid_argument(ss.str());
+    }
+
+    return std::move(m_parsed_info);
+}
+
+void HelmParser::add_extended_annotations(
+    const std::string_view extended_annotations)
+{
+    m_parsed_info.extended_annotations = extended_annotations;
+}
+
+void HelmParser::add_helm_version(const std::string_view helm_version)
+{
+    m_parsed_info.helm_version = helm_version;
+}
+
+void HelmParser::add_polymer_group(const std::string_view polymer_group_id,
+                                   const std::string_view items,
+                                   const bool is_polymer_union)
+{
+    m_parsed_info.polymer_groups.push_back(
+        {polymer_group_id, items, is_polymer_union});
+}
+
+void HelmParser::add_connection(connection&& connection)
+{
+    m_parsed_info.connections.push_back(connection);
+}
+
+void HelmParser::add_polymer(const std::string_view polymer_id,
+                             const std::string_view annotation)
+{
+    auto& polymer = m_parsed_info.polymers.back();
+    polymer.id = polymer_id;
+    polymer.annotation = annotation;
+}
+
+void HelmParser::add_monomer(const std::string_view monomer_id,
+                             const bool is_smiles, const bool is_branch,
+                             const bool is_list,
+                             const std::string_view annotation)
+{
+    auto& polymers = m_parsed_info.polymers;
+    if (polymers.empty() || !polymers.back().id.empty()) {
+        polymers.push_back({});
+    }
+    auto& polymer = polymers.back();
+    polymer.monomers.push_back(
+        {monomer_id, is_smiles, is_branch, is_list, annotation});
+}
+
+void HelmParser::add_monomer_with_id(const std::string_view monomer_id,
+                                     const std::string_view annotation)
+{
+    add_monomer(monomer_id, false, false, false, annotation);
+}
+
+void HelmParser::add_smiles_monomer(const std::string_view smiles,
+                                    const std::string_view annotation)
+{
+    add_monomer(smiles, true, false, false, annotation);
+}
+
+void HelmParser::add_monomer_list(const std::string_view monomer_list,
+                                  const std::string_view annotation)
+{
+    add_monomer(monomer_list, false, false, true, annotation);
+}
+
+void HelmParser::mark_branch_monomer(const size_t branch_group_size)
+{
+    auto& polymer = m_parsed_info.polymers.back();
+    polymer.monomers[polymer.monomers.size() - branch_group_size + 1]
+        .is_branch = true;
+}
+
+void HelmParser::mark_last_n_monomers_as_repeated(
+    const size_t repetition_size, const std::string_view num_repetitions,
+    const std::string_view annotation)
+{
+    auto& polymer = m_parsed_info.polymers.back();
+    polymer.repetitions.push_back({polymer.monomers.size() - repetition_size,
+                                   repetition_size, num_repetitions,
+                                   annotation});
+}
+
+void HelmParser::add_residue_name(const std::string_view monomer_id)
+{
+    auto& polymers = m_parsed_info.polymers;
+    if (polymers.empty() || !polymers.back().id.empty()) {
+        polymers.push_back({});
+    }
+    auto& polymer = m_parsed_info.polymers.back();
+    polymer.residue_names.insert(monomer_id);
+}
+void HelmParser::add_wildcard_or_unknown_residue()
+{
+    auto& polymers = m_parsed_info.polymers;
+    if (polymers.empty() || !polymers.back().id.empty()) {
+        polymers.push_back({});
+    }
+    auto& polymer = m_parsed_info.polymers.back();
+    polymer.wildcard_and_unknown_residues.insert(polymer.monomers.size() + 1);
+}
+
+bool HelmParser::hasErrors()
+{
+    return !m_errors.empty();
+}
+
+void HelmParser::saveError(const std::string_view& failed_token,
+                           const std::string& err_msg)
+{
+    const auto num_chars_processed =
+        std::distance(m_input.data(), failed_token.data()) + 1;
+    saveError(num_chars_processed, err_msg);
+}
+
+void HelmParser::saveError(const unsigned int num_chars_processed,
+                           const std::string& err_msg)
+{
+    // NOTE: If the input is very long, the pointer to the failed location
+    // becomes less useful. We should truncate the length of the error message
+    // to 101 chars.
+    static constexpr unsigned int error_size{101};
+    static constexpr unsigned int prefix_size{error_size / 2};
+    static auto truncate_input = [](const auto& input, const unsigned int pos) {
+        if ((pos >= prefix_size) && (pos + prefix_size) < input.size()) {
+            return input.substr(pos - prefix_size, error_size);
+        } else if (pos >= prefix_size) {
+            return input.substr(pos - prefix_size);
+        } else {
+            return input.substr(
+                0, std::min(input.size(), static_cast<size_t>(error_size)));
+        }
+    };
+
+    size_t num_dashes =
+        (num_chars_processed >= prefix_size ? prefix_size
+                                            : num_chars_processed - 1);
+    m_errors.push_back(fmt::format(
+        "Malformed HELM string: check for mistakes around position {}:\n"
+        "{}\n"
+        "{}^\n"
+        "{}",
+        num_chars_processed, truncate_input(m_input, num_chars_processed - 1),
+        std::string(num_dashes, '-'), err_msg));
+}
+
+} // namespace helm
---
+++ test/schrodinger/rdkit_extensions/test_common.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <cstdlib>
+#include <exception>
+#include <string>
+
+#include <filesystem>
+
+/**
+ * Predicate to check if an exception message contains a specific substring
+ */
+struct MsgSubstr {
+    MsgSubstr(const std::string& expected) : m_expected(expected)
+    {
+    }
+    bool operator()(const std::exception& ex) const
+    {
+        return std::string(ex.what()).find(m_expected) != std::string::npos;
+    }
+
+  private:
+    std::string m_expected;
+};
+
+/**
+ * Macro to check a specific exception type with a specific message substring
+ */
+#define TEST_CHECK_EXCEPTION_MSG_SUBSTR(statement, exception_type, substr) \
+    BOOST_CHECK_EXCEPTION(statement, exception_type, MsgSubstr(substr))
+
+/**
+ * Gets full path to a file in the testfiles directory in the source
+ * @param filename file found in the testfiles folder
+ * @return full filesystem path to that file
+ */
+std::string testfile_path(const std::string& filename)
+{
+    auto path = std::filesystem::path("..") / "test" / "testfiles" / filename;
+    if (std::getenv("SCHRODINGER_SRC")) {
+        path = std::filesystem::path(std::getenv("SCHRODINGER_SRC")) /
+               "mmshare" / "test" / "testfiles" / "rdkit_extensions" / filename;
+    }
+    if (!std::filesystem::exists(path)) {
+        throw std::runtime_error("File not found: " +
+                                 std::filesystem::absolute(path).string());
+    }
+    return path.string();
+}
+
+/**
+ * Set the default monomer db path
+ */
+void set_default_monomer_db_path()
+{
+    auto path = std::filesystem::path("..") / "data" / "core_monomerlib.db";
+    if (std::getenv("SCHRODINGER_SRC")) {
+        path = std::filesystem::path(std::getenv("SCHRODINGER_SRC")) /
+               "mmshare" / "data" / "helm" / "core_monomerlib.db";
+    }
+    if (!std::filesystem::exists(path)) {
+        throw std::runtime_error("Monomer database not found: " +
+                                 std::filesystem::absolute(path).string());
+    }
+
+    // Set the environment variable
+    auto name = "SCHRODINGER_DEFAULT_MONOMER_DB_PATH";
+    auto value = path.string();
+#ifdef _WIN32
+    _putenv_s(name, value.c_str());
+#else
+    int overwrite = 1;
+    setenv(name, value.c_str(), overwrite);
+#endif
+}
