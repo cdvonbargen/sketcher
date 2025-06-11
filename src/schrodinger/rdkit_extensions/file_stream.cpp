fda6f574092d8674d03cecc92015a4d7f152d88e
Chris Von Bargen <christopher.vonbargen@schrodinger.com>
2025-06-11 13:35:32 -0400

    Hussein patch

--- src/schrodinger/rdkit_extensions/file_stream.cpp
+++ src/schrodinger/rdkit_extensions/file_stream.cpp
@@ -143,7 +143,7 @@ std::string get_compressed_string(const std::string& data,
     if (compression_type == CompressionType::UNKNOWN) {
         return data;
     } else {
-        std::stringstream ss;
+        std::stringstream ss(std::ios_base::out | std::ios::binary);
         // use maybe_compressed_ostream as RAII
         {
             maybe_compressed_ostream os(ss, compression_type);
@@ -166,7 +166,7 @@ std::string get_decompressed_string(const std::string& data,
 
 std::string get_decompressed_string(const std::string& data)
 {
-    std::istringstream sstream(data);
+    std::istringstream sstream(data, std::ios_base::in | std::ios::binary);
     auto compression_type = get_compression_type(sstream);
     return get_decompressed_string(data, compression_type);
 }
