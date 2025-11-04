c3e994b190636c8daed2bed939901b37f9d77dd6
Chris Von Bargen <christopher.vonbargen@schrodinger.com>
2025-11-04 18:43:45 -0500

    draft

--- test/wasm/wasm_api.test.js
+++ test/wasm/wasm_api.test.js
@@ -10,7 +10,7 @@ test.describe('WASM Sketcher API', () => {
 
   // Test import and export for all formats
   const FORMATS = [
-    { format: 'AUTO_DETECT', skip: [true, "Doesn't make sense to test here"] },
+    { format: 'AUTO_DETECT', importUnsupported: true, exportUnsupported: true },
     { format: 'RDMOL_BINARY_BASE64' },
     { format: 'SMILES' },
     { format: 'EXTENDED_SMILES' },
@@ -25,7 +25,7 @@ test.describe('WASM Sketcher API', () => {
     { format: 'MOL2', exportUnsupported: true },
     { format: 'XYZ' },
     { format: 'MRV' },
-    { format: 'CDXML', skip: [true, "Format doesn't import correctly in WASM builds"] },
+    { format: 'CDXML', exportUnsupported: true },
     { format: 'HELM' },
     { format: 'FASTA_PEPTIDE', exportUnsupported: true },
     { format: 'FASTA_DNA', exportUnsupported: true },
@@ -33,30 +33,42 @@ test.describe('WASM Sketcher API', () => {
     { format: 'FASTA', importUnsupported: true },
   ];
 
-  FORMATS.forEach(({ format, skip, exportUnsupported, importUnsupported }) => {
-    test(`exporting/importing ${format}`, async ({ page }) => {
-      if (skip) {
-        test.skip(...skip);
-      } else {
-        test.skip(!!exportUnsupported, `${format} is import only`);
-      }
+  // Test exporting for formats that support export
+  FORMATS.forEach(({ format, exportUnsupported }) => {
+    test(`exporting ${format}`, async ({ page }) => {
+      test.skip(!!exportUnsupported, `${format} does not support export`);
+
       const exportedText = await page.evaluate((format) => {
         Module.sketcher_clear();
         Module.sketcher_import_text('C[C@H](N)C=O');
         const exported = Module.sketcher_export_text(Module.Format[format]);
         return exported;
       }, format);
-      expect(exportedText).toMatchSnapshot(`export_text_${format}.txt`);
+      expect(exportedText).toMatchSnapshot(`text-${format}.txt`);
+    });
+  });
 
-      // Test importing the exported value (round-trip test)
-      if (!importUnsupported) {
-        const importSuccessful = await page.evaluate((exportedText) => {
-          Module.sketcher_clear();
-          Module.sketcher_import_text(exportedText);
-          return !Module.sketcher_is_empty();
-        }, exportedText);
-        expect(importSuccessful).toBe(true);
-      }
+  // Test importing for formats that support import
+  FORMATS.forEach(({ format, importUnsupported }) => {
+    test(`importing ${format}`, async ({ page }, testInfo) => {
+      test.skip(!!importUnsupported, `${format} does not support import`);
+
+      // Read the snapshot file
+      const fs = require('fs');
+      const path = require('path');
+      const snapshotPath = path.join(
+        testInfo.snapshotDir,
+        `text-${format}.txt`
+      );
+      const inputText = fs.readFileSync(snapshotPath, 'utf8');
+
+      // Import and verify the sketcher is not empty
+      const importSuccessful = await page.evaluate((inputText) => {
+        Module.sketcher_clear();
+        Module.sketcher_import_text(inputText);
+        return !Module.sketcher_is_empty();
+      }, inputText);
+      expect(importSuccessful).toBe(true);
     });
   });
 
@@ -125,49 +137,4 @@ test.describe('WASM Sketcher API', () => {
     });
   });
 
-  test("Import CDXML from ketcher file", async ({ page }) => {
-    const cdxmlInput = `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE CDXML SYSTEM "http://www.cambridgesoft.com/xml/cdxml.dtd">
-<CDXML BondLength="30.000000" LabelFont="3" CaptionFont="4">
-    <fonttable id="1">
-        <font id="2" charset="utf-8" name="Arial"/>
-        <font id="3" charset="utf-8" name="Times New Roman"/>
-    </fonttable>
-    <colortable>
-        <color r="1" g="1" b="1"/>
-        <color r="0" g="0" b="0"/>
-        <color r="1" g="0" b="0"/>
-        <color r="1" g="1" b="0"/>
-        <color r="0" g="1" b="0"/>
-        <color r="0" g="1" b="1"/>
-        <color r="0" g="0" b="1"/>
-        <color r="1" g="0" b="1"/>
-        <color r="0.5" g="0.5" b="0.5"/>
-    </colortable>
-    <page HeightPages="1" WidthPages="1" BoundingBox="240.295486 161.250992 292.204529 101.249008">
-        <fragment id="4">
-            <n id="5" p="0.000000 15.003225"/>
-            <n id="6" p="51.909027 14.988670"/>
-            <n id="7" p="26.003637 -0.000000"/>
-            <n id="8" p="51.909027 45.016960"/>
-            <n id="9" p="0.000000 45.151600"/>
-            <n id="10" p="26.069126 60.001980"/>
-            <b id="11" B="7" E="5" Order="2"/>
-            <b id="12" B="5" E="9"/>
-            <b id="13" B="9" E="10" Order="2"/>
-            <b id="14" B="10" E="8"/>
-            <b id="15" B="8" E="6" Order="2"/>
-            <b id="16" B="6" E="7"/>
-        </fragment>
-    </page>
-</CDXML>`;
-
-    // Import the CDXML and extract SMILES
-    const exportedSmiles = await page.evaluate((cdxml) => {
-      Module.sketcher_import_text(cdxml);
-      return Module.sketcher_export_text(Module.Format.SMILES);
-    }, cdxmlInput);
-
-    expect(exportedSmiles).toBe("C1=CC=CC=C1");
-  });
 });
